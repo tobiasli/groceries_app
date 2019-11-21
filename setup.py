9e8e131b7f909fce831db3ab111d50831ad48283
Tobias Litherland <tobias.litherland@statkraft.com>
2019-11-21 21:07:38 +0100

    New version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='groceries_app-tobiasli',
-                 version='1.0.0',
+                 version='1.0.1',
                  description='App for compiling shopping lists.',
                  author='Tobias Litherland',
                  author_email='tobiaslland@gmail.com',
